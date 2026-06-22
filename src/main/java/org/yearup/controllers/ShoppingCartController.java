174131e4730f780764bcf9bc7638cccbbb5c9fe5
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-22 13:46:23 -0400

    Updaded the shopping cart controller, add appropriate annotations and return methods

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,33 +1,48 @@
 package org.yearup.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
+import org.yearup.repository.ShoppingCartRepository;
 import org.yearup.service.ShoppingCartService;
 import org.yearup.service.UserService;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
-// only logged in users should have access to these actions
-public class ShoppingCartController
-{
+@RestController
+@RequestMapping("/cart")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class ShoppingCartController {
+
     // a shopping cart controller depends on the service layer
     private ShoppingCartService shoppingCartService;
     private UserService userService;
 
 
+    //Constructor Injection
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+    }
 
-    // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
-    {
+
+
+    @GetMapping
+    public ShoppingCart getCart(Principal principal) {
+
         // get the currently logged in username
         String userName = principal.getName();
         // find database user by username
         User user = userService.getByUserName(userName);
         int userId = user.getId();
 
-        // use the shoppingCartService to get all items in the cart and return the cart
-        return null;
+        // get all items in the cart and return the cart
+        return shoppingCartService.getByUserId(userId);
     }
 
     // add a POST method to add a product to the cart - the url should be
