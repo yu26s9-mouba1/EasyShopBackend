d01db9efba4cb0c510fa6517d889afc389b76eb7
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-23 14:39:55 -0400

    Create the Order class with appropriate annotations, constructors and getters and setters

--- src/main/java/org/yearup/models/Order.java
+++ src/main/java/org/yearup/models/Order.java
@@ -1,4 +1,96 @@
 package org.yearup.models;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderId;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    private LocalDateTime date;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+
+    @Column(name = "shipping_name")
+    private BigDecimal shippingAmount =  BigDecimal.ZERO;
+
+    //Getters and setters
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public BigDecimal getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(BigDecimal shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
 }
+
