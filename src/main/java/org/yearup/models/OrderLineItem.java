a322ab2fcf4d264bf12ebf5d68c9b6564c5b512b
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-23 14:46:45 -0400

    Create the OrderLineItem class with appropriate fields, anotations, getters and setters

--- src/main/java/org/yearup/models/OrderLineItem.java
+++ src/main/java/org/yearup/models/OrderLineItem.java
@@ -1,4 +1,76 @@
 package org.yearup.models;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_Line_items")
 public class OrderLineItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_Line_item_id")
+    private int orderLineItemId;
+
+    @Column(name = "order_id")
+    private int orderId;
+
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "sales_price")
+    private BigDecimal salesPrice;
+
+    private int quantity;
+
+    private BigDecimal discount = BigDecimal.ZERO;
+
+    public int getOrderLineItemId() {
+        return orderLineItemId;
+    }
+
+    public void setOrderLineItemId(int orderLineItemId) {
+        this.orderLineItemId = orderLineItemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(BigDecimal salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
 }
