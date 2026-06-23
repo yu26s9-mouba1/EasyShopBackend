20153530d0af249fb202eca6bc33b0bd438c18bd
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-23 14:28:55 -0400

    Start the order and orderlineRepository classes

--- src/main/java/org/yearup/repository/OrderLineItemRepository.java
+++ src/main/java/org/yearup/repository/OrderLineItemRepository.java
@@ -1,4 +1,11 @@
 package org.yearup.repository;
 
-public class OrderLineItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.yearup.models.OrderLineItem;
+
+@Repository
+public interface OrderLineItemRepository extends JpaRepository<OrderLineItem, Integer> {
+
+
 }
--- src/main/java/org/yearup/repository/OrderRepository.java
+++ src/main/java/org/yearup/repository/OrderRepository.java
@@ -1,4 +1,11 @@
 package org.yearup.repository;
 
-public class OrderRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.yearup.models.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+
 }
