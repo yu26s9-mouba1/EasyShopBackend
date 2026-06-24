6d6aaaa9cbd605b84ff3afaf9e9da1623a584a4d
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-24 12:24:23 -0400

    Update OrderService, add endpoints then create OrdersController class with appropriate fields and endpoints

---
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -0,0 +1,38 @@
+package org.yearup.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.yearup.models.User;
+import org.yearup.service.OrderService;
+import org.yearup.service.UserService;
+import org.yearup.models.Order;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/orders")
+public class OrdersController {
+
+    private final OrderService orderService;
+    private final UserService userService;
+
+    //Contructor
+    public OrdersController(OrderService orderService, UserService userService) {
+        this.orderService = orderService;
+        this.userService = userService;
+    }
+
+
+    //CheckOut Method endpoint
+    @PostMapping
+    public ResponseEntity<Order> checkout(Principal principal) {
+        String username = principal.getName();
+        User user = userService.getByUserName(username);
+
+        Order order = orderService.checkout(user.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
+}
--- src/main/java/org/yearup/service/OrderService.java
+++ src/main/java/org/yearup/service/OrderService.java
@@ -1,8 +1,12 @@
 package org.yearup.service;
+import org.yearup.models.Order;
+import java.security.Principal;
 
+import org.springframework.stereotype.Service;
 import org.yearup.repository.OrderLineItemRepository;
 import org.yearup.repository.OrderRepository;
 
+@Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -12,15 +16,19 @@ public class OrderService {
 
 
     //Constructor
-    public OrderService(OrderRepository orderRepository, OrderLineItemRepository orderLineItemRepository, ShoppingCartService shoppingCartService, ProfileService profileService) {
+    public OrderService( OrderRepository orderRepository,
+                         OrderLineItemRepository orderLineItemRepository,
+                         ShoppingCartService shoppingCartService,
+                         ProfileService profileService) {
         this.orderRepository = orderRepository;
         this.orderLineItemRepository = orderLineItemRepository;
         this.shoppingCartService = shoppingCartService;
         this.profileService = profileService;
     }
 
-    //Checkout Method
-//    public Order checkout(int  userId) {
-//
-//    }
+   // Checkout Method
+    public Order checkout(int  userId) {
+        return null;
+
+    }
 }
