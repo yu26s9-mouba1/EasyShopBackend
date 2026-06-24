a0922a754565f4dae47759a0cddd667e3444ae74
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-24 11:55:00 -0400

    Create the orderService class with apropriate fields and the checkout method

---
+++ src/main/java/org/yearup/service/OrderService.java
@@ -0,0 +1,26 @@
+package org.yearup.service;
+
+import org.yearup.repository.OrderLineItemRepository;
+import org.yearup.repository.OrderRepository;
+
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderLineItemRepository orderLineItemRepository;
+    private final ShoppingCartService shoppingCartService;
+    private final ProfileService profileService;
+
+
+    //Constructor
+    public OrderService(OrderRepository orderRepository, OrderLineItemRepository orderLineItemRepository, ShoppingCartService shoppingCartService, ProfileService profileService) {
+        this.orderRepository = orderRepository;
+        this.orderLineItemRepository = orderLineItemRepository;
+        this.shoppingCartService = shoppingCartService;
+        this.profileService = profileService;
+    }
+
+    //Checkout Method
+//    public Order checkout(int  userId) {
+//
+//    }
+}
